ac81cf9443a8e8c6ebca6e724e937b2f5c3d11d3
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-31 12:52:32 -0800

    make the constructors

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+//add constructor
+function Order() {
+  this.pizzas = [],;
+  this.currentId = 0;
+}
+function Pizza(size, topping, price) {
+  this.size = size,;
+  this.topping1 = topping,;
+  this.price = price;
+}
