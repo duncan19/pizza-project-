392e3bbe4137aa5a99af65c8986af7883e9bbee0
Duncan Robbins <duncanrobbins19@gmail.com>
2020-01-31 13:30:03 -0800

    add two prototype methods

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,11 @@ function Pizza(size, topping, price) {
   this.topping1 = topping,;
   this.price = price;
 }
+Order.prototype.addPizza = function(pizza) {
+  contact.id = this.assignId();
+  this.pizzas.push(pizza);
+}
+Order.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
